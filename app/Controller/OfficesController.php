89591408027ba112b2b4fb0c8da7c7521eaf56e3
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2012-05-16 04:27:57 -0500

    revs

--- app/Controller/OfficesController.php
+++ app/Controller/OfficesController.php
@@ -31,6 +31,83 @@ class OfficesController extends AppController {
 		}
 		$this -> set('office', $this -> Office -> read(null, $id));
 	}
+	
+	public function csvUpload() {
+		if($this -> request -> is('post')) {
+			if(isset($this -> request -> data['Office']['csv']['error']) && $this -> request -> data['Office']['csv']['error'] > 0) {
+				$this -> Session -> setFlash('Ha ocurrido un error con la subida del archivo. Por favor, intente de nuevo.');
+				$this -> redirect(array('action' => 'add'));
+			} else {
+				if($this -> request -> data['Office']['csv']['type'] != 'text/csv') {
+					$this -> Session -> setFlash('El archivo que ha subido no es formato CSV. Por favor, intente de nuevo.');
+					$this -> redirect(array('action' => 'add'));
+				} else {
+					$message = $this -> csvFileRead($this -> request -> data['Office']['csv']['tmp_name']);
+					$this -> Session -> setFlash($message['message'], $message['crud']);
+					if($message['crud'] == 'crud/success') {
+						$this -> redirect(array('action' => 'index'));
+					} else {
+						$this -> redirect(array('action' => 'add'));
+					}
+				}
+			}
+		} else {
+			$this -> redirect(array('action' => 'index'));
+		}
+	}
+	
+	private function csvFileRead($path = null) {
+		App::uses('File', 'Utility');
+		$message = array();
+		if($path) {
+			$file = new File($path, false, 0644);
+			$data = null;
+			if ($file -> exists() && $file -> readable()) {
+				$data = $file -> read();
+			}
+			$file -> close();
+			$registroCreado = false;
+			if ($data) {
+				$data = explode(chr(10), $data);
+				$tmpData = array();
+				foreach($data as $key => $value) {
+					if(!empty($value)) $tmpData[] = $value;
+				}
+				$data = $tmpData;
+				$delimiter = ',';
+				$headers = explode($delimiter, $data[0]);
+				unset($data[0]);
+				foreach($data as $key => $value) {
+					$datosOficina = explode($delimiter, $value);
+					$oficina = array('Office' => array());
+					for($i = 0; $i < count($headers); $i+=1) {
+						$oficina['Office'][$headers[$i]] = $datosOficina[$i];
+					}
+					$this -> Office -> create();
+					if(!$this -> Office -> save($oficina)) {
+						$tmpError = $this -> Office -> invalidFields();
+						$errorMessage = '<p>Para la oficina "' . $oficina['Office']['nombre'] . '" ocurrio lo siguiente:</p>';
+						foreach ($tmpError as $field => $error) {
+							$errorMessage .= '<p>' . $field . ' > ' . $error[0] . '</p>';
+						}
+						$message['message'] = '<p>Error al tratar de crear un registro</p>' . '<p><< Información del error >></p><p>' . $errorMessage . '</p>';
+						$message['crud'] = 'crud/error';
+						return $message;
+					} else {
+						$registroCreado = true;
+					}
+				}
+			}
+			if($registroCreado) {
+				$message['message'] = 'Se registraron las oficinas.';
+				$message['crud'] = 'crud/success';
+			} else {
+				$message['message'] = 'No se creó al menos una de las oficinas o no habia registros.';
+				$message['crud'] = 'crud/error';
+			}
+			return $message;
+		}
+	}
 
 	/**
 	 * add method
@@ -41,6 +118,7 @@ class OfficesController extends AppController {
 		$this -> layout = "mapas";
 		if ($this -> request -> is('post')) {
 			$this -> Office -> create();
+			$this -> request -> data['Office']['page_created'] = true;
 			if ($this -> Office -> save($this -> request -> data)) {
 				$this -> Session -> setFlash(__('Se guardó la oficina'), 'crud/success');
 				$this -> redirect(array('action' => 'index'));
--- app/View/Offices/add.ctp
+++ app/View/Offices/add.ctp
@@ -1,21 +1,31 @@
 <div class="offices form dos-col">
-<?php echo $this->Form->create('Office');?>
+	<?php echo $this->Form->create('Office');?>
 	<fieldset>
 		<legend><?php echo __('Añadir Oficina'); ?></legend>
-	<?php
-		echo $this->Form->input('office_type_id', array('label' => 'Tipo De Oficina'));
-		echo $this->Form->input('city_id',array('label' => 'Ciudad', 'class'=>'cityId'));
-		echo $this->Form->input('nombre');
-		echo $this->Form->input('direccion', array('label'=>'Dirección'));
-		echo $this->Form->input('descripcion', array('label' => 'Descripción'));
-		echo $this->Form->input('latitud',array('class'=>'lat'));
-		echo $this->Form->input('longitud',array('class'=>'lng'));
-	?>
+		<?php
+			echo $this->Form->input('office_type_id', array('label' => 'Tipo De Oficina'));
+			echo $this->Form->input('city_id',array('label' => 'Ciudad', 'class'=>'cityId'));
+			echo $this->Form->input('nombre');
+			echo $this->Form->input('direccion', array('label'=>'Dirección'));
+			echo $this->Form->input('descripcion', array('label' => 'Descripción'));
+			echo $this->Form->input('latitud',array('class'=>'lat'));
+			echo $this->Form->input('longitud',array('class'=>'lng'));
+		?>
 	</fieldset>
-<?php echo $this->Form->end(__('Guardar'));?>
+	<?php echo $this->Form->end(__('Guardar'));?>
 <div class="second-col">
 	<div id="map_offices" class='map'>
 		
 	</div>
 </div>
+</div>
+<div class="offices form csv-upload">
+	<?php echo $this->Form->create('Office', array('type' => 'file', 'action' => 'csvUpload'));?>
+	<fieldset>
+		<legend><?php echo __('Añadir oficinas mediante archivo CSV'); ?></legend>
+	<?php
+		echo $this->Form->input('csv', array('label' => 'Archivo CSV', 'type' => 'file'));
+	?>
+	</fieldset>
+	<?php echo $this->Form->end(__('Subir archivo'));?>
 </div>
